e341327b22c31b37f0fef46e29720335a598052d
Cibele Goudinho <cibelegoudinho13@gmail.com>
2022-12-03 23:29:03 -0300

    Adicionando front

---
+++ front/src/App.jsx
@@ -0,0 +1,122 @@
+import React,{useState} from 'react'
+import MapImage from '../src/assets/map.png';
+import { mapLevels, allCitiesMap } from './citiesMock';
+import './App.css';
+
+function App() {
+  const [userLevel, setUserLevel] = useState('')
+  const [cities, setCities] = useState([])
+  const [route, setRoute] = useState([])
+  const [userCity, setUserCity] = useState('')
+  const [destinyCity, setDestinyCity] = useState('')
+  const [showCities, setShowCities] = useState(false)
+  const [loadingResult, setLoadingResult] = useState(false)
+
+  function handleChangeUserLevel(value){
+    setUserLevel(value)
+  }
+
+  function handleSubmitLevel(){
+
+    let response = mapLevels.filter(city => Number(userLevel) >= city.minLevel)
+    
+    console.log("handleSubmitLevel", response)
+    setCities(response)
+    setShowCities(true)
+  }
+
+  function handleChangeCity(value){
+    setUserCity(value)
+  }
+
+  function handleChangeDestinyCity(value){
+    setDestinyCity(value)
+  }
+
+  function handleSubmitCity(){
+    // fazer fetch get para api
+    fetch(`http://localhost:8000/graph?city_i=${userCity.toLowerCase()}&city_d=${destinyCity.toLowerCase()}`)
+    .then((response) => response.json())
+    .then((data) => {
+      setLoadingResult(true)
+      setRoute(data)
+    });
+    
+  }
+
+  function handleResetForm(){
+    setUserCity('')
+    setDestinyCity('')
+    setUserLevel('')
+    setShowCities(false)
+    setLoadingResult(false)
+  }
+
+  return (
+    <div className="App">
+      <div className='content'>
+        <div className='map'>
+          <img src={MapImage} className="odyssey-map" alt="map" />
+        </div>
+        <div className='form'>
+          <h1 className='title'>
+            Odyssey Level Route
+          </h1>
+          <div id="step-level" className='widget-question'>
+            <h3>Insira seu nível (1-19):</h3>
+            
+            <div className='input-container'>
+              <input onChange={(e)=> handleChangeUserLevel(e.target.value)}
+                type="number" 
+                min="1" 
+                max="19" 
+                value={userLevel} 
+                name="level" 
+                className="input"/>
+
+              <button onClick={handleSubmitLevel} className="button">Enviar</button>
+            </div>
+          </div>
+
+          {showCities && (
+            <div id="step-city" className='widget-question'>
+              <h3>Selecione as cidades:</h3>
+              {/* <p>{cities.map(city => city.name).join(', ')}</p> */}
+            
+              <div className='input-container'>
+                <select value={userCity} name="cities" id="cities" onChange={(e)=> handleChangeCity(e.target.value)}>
+                  <option>Selecione uma cidade de início</option>
+                  {cities?.map(city => 
+                    <option key={city.name} value={city.name}>
+                      {city.name}
+                    </option>
+                  )}
+                </select>
+                <select value={destinyCity} name="allCities" id="allCities" onChange={(e)=> handleChangeDestinyCity(e.target.value)}>
+                  <option>Selecione uma cidade de destino</option>
+                  {allCitiesMap?.map(city => 
+                    <option key={city} value={city}>
+                      {city}
+                    </option>
+                  )}
+                </select>
+                <button onClick={handleSubmitCity} className="button">Enviar</button>
+              </div>
+            </div>
+          )}
+
+          {loadingResult && (
+            <div id="step-route" className='widget-question'>
+              <h3>A melhor rota para {destinyCity} considerando a distância e o nível das cidades é:</h3>
+              <p>{route.join(', ')}</p>
+              <button onClick={handleResetForm}>Refazer</button>
+            </div>
+          )}
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
---
+++ front/src/citiesMock.js
@@ -0,0 +1,23 @@
+export const mapLevels = [
+    {name:'Kephallonia', minLevel:1, maxLevel:6},
+    {name:'Megaris', minLevel:6, maxLevel:9},
+    {name:'Phokis', minLevel:9, maxLevel:13},
+    {name:'Malis', minLevel:12, maxLevel:14},
+    {name:'Lokris', minLevel:12, maxLevel:14},
+    {name:'Euboea', minLevel:12, maxLevel:16},
+    {name:'Skyros', minLevel:12, maxLevel:16},
+    {name:'Andros', minLevel:13, maxLevel:13},
+    {name:'Attika', minLevel:15, maxLevel:19},
+    {name:'Seriphos', minLevel:17, maxLevel:19},
+    {name:'Keos', minLevel:17, maxLevel:19},
+    {name:'Korinthia', minLevel:19, maxLevel:23},
+    {name:'Argolis', minLevel:18, maxLevel:22},
+    {name:'Hydrea', minLevel:18, maxLevel:20},
+    {name:'Melos', minLevel:18, maxLevel:20},
+]
+
+export const allCitiesMap = ["Achaia", "Anaphi", "Andros", "Argolis", "Arkadia", "Attika", "Boeotia", "Chios",
+"Delos", "Elis", "Euboea", "Hydrea", "Keos", "Kephallonia", "Korinthia", "Kos", "Kythera Island", "Lakonia",
+"Lemnos", "Lesbos", "Lokris", "Makedonia", "Malis", "Megaris", "Melos", "Messara", "Messenia", "Mykonos",
+"Naxos", "Nisyros", "Paros", "Pephka", "Phokis", "Samos", "Seriphos", "Skyros", "Thasos", "Thera"
+]
